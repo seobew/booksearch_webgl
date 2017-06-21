ba48f2468876bc367fbbc3afc45c863ff2da5d24
KimISeob <seobew@gmail.com>
2017-06-21 09:20:12 +0900

    demo complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,9 +15,17 @@ class HomeController < ApplicationController
       end
     end
   end
+
   def obj
-    shelf_id = params[:shelf_id]
-    @shelf = Shelf.find(shelf_id)
-    puts @shelf
+    @shelf_id = params[:shelf_id]
+    puts @shelf_id
+    @shelf = Shelf.find(@shelf_id)
+    @shelf_x = @shelf.x-50
+    if @shelf.z<0 
+      @shelf_z = @shelf.z-360  
+    else
+      @shelf_z = @shelf.z+270
+    end
+    
   end
 end
